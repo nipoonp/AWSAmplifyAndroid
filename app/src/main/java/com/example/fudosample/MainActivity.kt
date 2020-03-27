aa11c9328dfaa8d20f2884dbfb0336af19990a99
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2020-03-27 23:28:53 +1300

    Add logout

--- app/src/main/java/com/example/fudosample/MainActivity.kt
+++ app/src/main/java/com/example/fudosample/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.fudosample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.amazonaws.mobile.client.AWSMobileClient
+import kotlinx.android.synthetic.main.activity_forgot_password.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        log_out_button.setOnClickListener {
+            AWSMobileClient.getInstance().signOut()
+
+            val intent = Intent(this@MainActivity, AuthenticationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
